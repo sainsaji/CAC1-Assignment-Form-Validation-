9d45e6efa56ba155484cacdb4ad97a020024c8b9
sainsaji <95070425+sainsaji@users.noreply.github.com>
2022-01-29 01:27:09 +0530

    fixed till email,password not working

--- validation.js
+++ validation.js
@@ -8,111 +8,139 @@ function validateAlphabets(event) {
 	  return false;
 	}
 
+	function setFocus()
+	{
+		document.getElementById("name").focus();
+	}
+
+
 	function validation() 
 	{
-        var name = document.getElementById("name").value;
-        var user = document.getElementById("user").value;
-        var pass = document.getElementById("pass").value;
-        var emails = document.getElementById("emails").value;
+		if(document.getElementById("name").value.length == 0)
+		{
+			document.getElementById("Name").innerHTML =
+		"Name field is empty";
+			document.getElementById("name").focus();
+		return false;
+		}
+		else
+		{
+			document.getElementById("Name").innerHTML =
+		" ";
+		}
 
-        if (name == "") {
-          document.getElementById("Name").innerHTML =
-            "  Please fill the Name field";
-          return false;
-        }
-        var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,<>\/?]+/;
+		var name = document.getElementById("name").value;
+		var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,<>\/?]+/;
+		if(format.test(name)){
+			document.getElementById("Name").innerHTML =
+			  "No Special Characters allowed";
+			  document.getElementById("name").focus();
+			  return false;
+		  } else {
+			document.getElementById("Name").innerHTML =
+			  " ";
+		  }
 
-        if(format.test(name)){
-          document.getElementById("Name").innerHTML =
-            "No Special Characters allowed";
-        } else {
-          document.getElementById("Name").innerHTML =
-            " ";
-        }
-        if (emails == "") {
-          document.getElementById("email").innerHTML =
-            "  Please fill the email id field";
-          return false;
-        }
-        if (emails.indexOf("@automania.in") <= 0) {
-          document.getElementById("email").innerHTML = "  Invalid Email";
-          return false;
-        }
-        else
-        {
-          document.getElementById("email").innerHTML = " ";
-        }
+		if(document.getElementById("address").value.length == 0)
+		{
+			document.getElementById("Address").innerHTML =
+		"Adress field is empty";
+			document.getElementById("address").focus();
+		return false;
+		}
+		else
+		{
+			document.getElementById("Address").innerHTML =
+		" ";
+		}
 
-        if (
-          emails.charAt(emails.length - 4) != "." &&
-          emails.charAt(emails.length - 3) != "."
-        ) {
-          document.getElementById("email").innerHTML = "  Invalid Email";
-          return false;
-        }
+		if(document.getElementById("email").value.length == 0)
+		{
+			document.getElementById("Email").innerHTML =
+		"Email Address field is empty";
+			document.getElementById("email").focus();
+		return false;
+		}
+		else
+		{
+			document.getElementById("Email").innerHTML =
+		" ";
+		}
 
-        if (user == "") {
-          document.getElementById("user").innerHTML =
-            "  Please fill the username field";
-          return false;
-        }
-        if (user.length <= 3 || user.length > 20) {
-          document.getElementById("user").innerHTML =
-            "  Username lenght must be between 3 and 20";
-          return false;
-        }
-        if (!isNaN(user)) {
-          document.getElementById("user").innerHTML =
-            "  only characters are allowed";
-          return false;
-        }
+		format = /^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@christuniversity.in$/
+		email = document.getElementById("email").value;
+		if(format.test(email)){
+			document.getElementById("Email").innerHTML =
+			  "Enter valid email address";
+			  document.getElementById("email").focus();
+			  return false;
+		  } else {
+			document.getElementById("Email").innerHTML =
+			  " ";
+		  }
 
-        if (pass == "") {
-          document.getElementById("pass").innerHTML =
-            "  Please fill the password field";
-          return false;
-        }
-        if (pass.length <= 8 || pass.length > 20) {
-          document.getElementById("pass").innerHTML =
-            "Passwords lenght must be between  8 and 20";
-          return false;
-          
-        
-        }
+		  if (email.indexOf("@test") <= 0) {
+			document.getElementById("Email").innerHTML = "  Invalid Email";
+			document.getElementById("email").focus();
+			return false;
+		  }
+		  else
+		  {
+			document.getElementById("Email").innerHTML = " ";
+		  }
 
-        re = /[0-9]/;
+		if(document.getElementById("user").value.length == 0)
+		{
+			document.getElementById("User").innerHTML =
+		"User field is empty";
+			document.getElementById("user").focus();
+		return false;
+		}
+		else
+		{
+			document.getElementById("User").innerHTML =
+		" ";
+		}
+
+		if(document.getElementById("pass").value.length == 0)
+		{
+			document.getElementById("Pass").innerHTML =
+		"Password is empty";
+			document.getElementById("pass").focus();
+		return false;
+		}
+		else
+		{
+			document.getElementById("Pass").innerHTML =
+		" ";
+		}
+		pass = document.getElementById("pass").value
+		
+		  if (pass.length <= 8 || pass.length > 20) {
+			document.getElementById("Pass").innerHTML =
+			  "Passwords lenght must be between  8 and 20";
+			  document.getElementById("user").focus();
+			return false;
+		  }
+
+		  re = /[0-9]/;
         if(!re.test(pass)) {
-          document.getElementById("pass").innerHTML =
+          document.getElementById("Pass").innerHTML =
             "Password should contain a number";
-          return false;
         pass.focus();
         return false;
         }
 
-        re = /[a-z]/;
-        if(!re.test(pass)) {
-          document.getElementById("pass").innerHTML =
-            "Password should contain a lowercase character";
-          return false;
-        pass.focus();
-        return false;
-        }
-
-        re = /[A-Z]/;
-        if(!re.test(pass)) {
-          document.getElementById("pass").innerHTML =
-            "Password should contain a uppercase character";
-          return false;
-        pass.focus();
-        return false;
-        }
-
-        var re = /[!@#\$%\^\&*\)\(+=._-]/
-        if(!re.test(pass)) {
-          document.getElementById("pass").innerHTML =
-            "Password should contain a special character";
-          return false;
-        pass.focus();
-        return false;
-        }
+		
+			var radios = document.getElementsByName("radio");
+	   
+			for (var i = 0, len = radios.length; i < len; i++) {
+				 if (radios[i].checked) {
+					 return true;
+				 }
+			}
+			document.getElementById("recheck").innerHTML =
+		"Select one radio";
+			return false;
+		
 	}
